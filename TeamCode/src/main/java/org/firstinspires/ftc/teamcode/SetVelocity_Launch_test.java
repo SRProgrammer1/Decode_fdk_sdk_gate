8c216678a04505c5ddcf605e53e9fec81d89be1e
Programer0112 <s97103483@gmail.com>
2025-11-13 12:36:29 -0500

    TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetVelocity_Launch_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetVelocity_Launch_test.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.mechanisms.FlyWheel_Launch_SetVelocity_Debug;
 
+import org.firstinspires.ftc.teamcode.mechanisms.FlyWheel_Launch_SetVelocity_test;
+
+//@Disabled
 @TeleOp(name = "SetVelocity Launch test", group = "TeleOp")
 public class SetVelocity_Launch_test extends OpMode {
 
-    FlyWheel_Launch_SetVelocity_Debug launch = new FlyWheel_Launch_SetVelocity_Debug();
+    FlyWheel_Launch_SetVelocity_test launch = new FlyWheel_Launch_SetVelocity_test();
     double targetRPM = 0;
 
     @Override
@@ -19,21 +21,19 @@ public class SetVelocity_Launch_test extends OpMode {
 
     @Override
     public void loop() {
-        // Choose base target RPM
         if (gamepad1.right_bumper) {
-            targetRPM = 3000;
+            targetRPM = 6000;
         } else if (gamepad1.right_trigger > 0.5) {
-            targetRPM = 2000;
+            targetRPM = 3000;
         } else if (gamepad1.left_bumper) {
-            targetRPM = 1000;
+            targetRPM = 1500;
         } else if (gamepad1.left_trigger > 0.5) {
             targetRPM = 0;
         }
 
-        // Use the stabilizing version instead of raw setVelocity
-        launch.stabilizeVelocity(targetRPM);
+        // Apply target RPM (motor2 will spin opposite automatically)
+        launch.setVelocityRPM(targetRPM);
 
-        telemetry.addData("Battery", launch.getBatteryVoltage());
         telemetry.addData("Target RPM", targetRPM);
         telemetry.addData("Motor1 Velocity", launch.getMotor1Velocity());
         telemetry.addData("Motor2 Velocity", launch.getMotor2Velocity());
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/FlyWheel_Launch_SetVelocity_test.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+
+public class FlyWheel_Launch_SetVelocity_test {
+
+    private DcMotorEx motor1, motor2;
+    private static final double TICKS_PER_REV = 25.0;
+
+    public double getMotor1Velocity() { return motor1.getVelocity(); }
+    public double getMotor2Velocity() { return motor2.getVelocity(); }
+
+    public void init(HardwareMap hwMap) {
+        motor1 = hwMap.get(DcMotorEx.class, "fly_wheel_1");
+        motor2 = hwMap.get(DcMotorEx.class, "fly_wheel_2");
+
+        //motor1.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        //motor2.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        motor1.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        motor2.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        // Set directions opposite for mirror spinning
+        motor1.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        /* How to Fine-Tune
+        Adjust F first:
+        Too slow → increase F slightly (+0.5 each test)
+        Overshooting → decrease F slightly (-0.5 each test)
+
+        Adjust P if needed:
+        Oscillations → lower P
+        Slow response → increase P slightly */
+
+        PIDFCoefficients pidf = new PIDFCoefficients(8.0, 0.0, 1.0, 0.0);
+
+        motor1.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidf);
+        motor2.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidf);
+    }
+
+    /** Set motor RPM (mirror image achieved by opposite directions) */
+    public void setVelocityRPM(double rpm) {
+        double velocity = rpmToTicksPerSecond(rpm);
+        motor1.setVelocity(velocity);
+        motor2.setVelocity(velocity); // same velocity, direction handles mirroring
+    }
+
+    private double rpmToTicksPerSecond(double rpm) {
+        return (rpm * TICKS_PER_REV) / 60.0;
+    }
+}
